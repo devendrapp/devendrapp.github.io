147e06d85000f8cfe9e2265914827344c97eef8b
Devendra Panchwagh <devendrapp@gmail.com>
2025-07-07 03:06:45 +0530

    ++

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
+const CACHE_NAME = 'door-chitra-vani-cache-070725'; // Update the version number
+const urlsToCache = [
+  '/',
+  '/index.html',
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('door-chitra-vani-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        // Add other essential files here
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
@@ -13,6 +16,16 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName))
+      );
     })
   );
 });
