35d61bdff817ca7f48e793adb27692261580096f
Devendra Panchwagh <devendrapp@gmail.com>
2025-07-24 15:35:11 +0530

    ++

--- sw.js
+++ sw.js
@@ -13,14 +13,18 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', event => {
+    if (event.request.cache === 'reload') {
+        console.log('Bypassing cache for:', event.request.url);
+        event.respondWith(fetch(event.request));
+        return;
+    }
 
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
