22d228805d0118b15c196bbf57e2feed8b68fca5
Devendra Panchwagh <devendrapp@gmail.com>
2025-08-12 17:28:54 +0530

    ++

--- scripts/init.js
+++ scripts/init.js
@@ -4,11 +4,21 @@ function loadDefaultItems() {
   localStorage.setItem("0000_default_categories", "श्री,🕉,📰,📻,🚩,🏵️");
 
   localStorage.setItem(
+    "0000_quick_search_buttons",
+    "🏵️,📰,📻,🖥️,🎙️,🎤,श्री,🕉,🙏,🚩,🐤,🎬,📀,🤣,🎵,✈️,🌧️,🎧,❤️,💔,🎶,🎸,🐅"
+  );
+
+  localStorage.setItem(
     "🏵️ राम रक्षा स्तोत्र | Shree Ram Raksha",
     "shree/stotra.html?id=RamRaksha"
   );
   localStorage.setItem(
     "🏵️ मारुती स्तोत्र | Maruti Stotra",
     "shree/stotra.html?id=MarutiStotra"
+  );
+
+  localStorage.setItem(
+    "श्री ​गणपती आरती | Ganapati Aarti",
+    "shree/stotra.html?id=GanapatiAarti"
   );
 }
