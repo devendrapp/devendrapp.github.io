20f706d47cf71a8892fe6015a0bcda00057e3b69
Devendra Panchwagh <devendrapp@gmail.com>
2025-08-13 13:38:45 +0530

    ++

--- scripts/init.js
+++ scripts/init.js
@@ -6,35 +6,35 @@ function loadDefaultItems() {
     "🌺,🏵️,🌼,श्री,🕉,🚩,📰,📻,🖥️,🎙️,🎤,🙏,🐤,🎬,📀,🤣,🎵,✈️,🌧️,🎧,❤️,💔,🎶,🎸,🐅"
   );
 
-  localStorage.setItem("🌺 श्री ​गणपती आरती | Ganapati Aarti","shree/stotra.html?id=GanapatiAarti");
-  localStorage.setItem("🌺 श्री दुर्गादेवीची आरती | Durga Devi Aarti","shree/stotra.html?id=DurgaAarti");
-  localStorage.setItem("🌺 श्री शंकराची आरती | Shankar Aarti","shree/stotra.html?id=ShankarAarti");
-  localStorage.setItem("🌺 श्री महालक्ष्मीची आरती | Mahalaxmi Aarti","shree/stotra.html?id=MahalaxmiAarti");
-  localStorage.setItem("🌺 श्री दत्ताची आरती | Dattachi Aarti","shree/stotra.html?id=DattachiAarti");
-  localStorage.setItem("🌺 श्री विठोबाची आरती | Vithoba Aarti","shree/stotra.html?id=VithobaAarti");
-  localStorage.setItem("🌺 श्री गजानन महाराजांची आरती | Gajanan Maharaj Aarti","shree/stotra.html?id=GajananMaharajAarti");
-  localStorage.setItem("🌺 श्री खंडेरायाची आरती | Khandoba Aarti","shree/stotra.html?id=KhandobaAarti");
-  localStorage.setItem("🌺 घालीन लोटांगण | Ghalin Lotangan (Aarti)","shree/stotra.html?id=GhalinLotangan");
-  localStorage.setItem("🌺 मंत्र पुष्पांजलि | Mantra Pushpanjali (Aarti)","shree/stotra.html?id=MantraPushpanjali");
+  localStorage.setItem("🌺 श्री ​गणपती आरती | Ganapati Aarti","shree/stotra.html?id=ga");
+  localStorage.setItem("🌺 श्री दुर्गादेवीची आरती | Durga Devi Aarti","shree/stotra.html?id=dga");
+  localStorage.setItem("🌺 श्री शंकराची आरती | Shankar Aarti","shree/stotra.html?id=sa");
+  localStorage.setItem("🌺 श्री महालक्ष्मीची आरती | Mahalaxmi Aarti","shree/stotra.html?id=ma");
+  localStorage.setItem("🌺 श्री दत्ताची आरती | Dattachi Aarti","shree/stotra.html?id=da");
+  localStorage.setItem("🌺 श्री विठोबाची आरती | Vithoba Aarti","shree/stotra.html?id=va");
+  localStorage.setItem("🌺 श्री गजानन महाराजांची आरती | Gajanan Maharaj Aarti","shree/stotra.html?id=gma");
+  localStorage.setItem("🌺 श्री खंडेरायाची आरती | Khandoba Aarti","shree/stotra.html?id=ka");
+  localStorage.setItem("🌺 घालीन लोटांगण | Ghalin Lotangan (Aarti)","shree/stotra.html?id=gl");
+  localStorage.setItem("🌺 मंत्र पुष्पांजलि | Mantra Pushpanjali (Aarti)","shree/stotra.html?id=mp");
 
-  localStorage.setItem("🌼 श्री राम रक्षा स्तोत्र | Shree Ram Raksha","shree/stotra.html?id=RamRaksha");
-  localStorage.setItem("🌼 मारुती स्तोत्र | Maruti Stotra","shree/stotra.html?id=MarutiStotra");
-  localStorage.setItem("🌼 शुभं करोति कल्याणम स्तोत्र | Shubham Karoti Stotra","shree/stotra.html?id=ShubhamKaroti");
+  localStorage.setItem("🌼 श्री राम रक्षा स्तोत्र | Shree Ram Raksha","shree/stotra.html?id=rr");
+  localStorage.setItem("🌼 मारुती स्तोत्र | Maruti Stotra","shree/stotra.html?id=ms");
+  localStorage.setItem("🌼 शुभं करोति कल्याणम स्तोत्र | Shubham Karoti Stotra","shree/stotra.html?id=sk");
   localStorage.setItem("🌼 श्री गणपती स्तोत्र | Ganapati Stotra","shree/stotra.html?id=GanapatiStotra");
-  localStorage.setItem("🌼 श्री अन्नपूर्णा स्तोत्र | Annapurna Stotra","shree/stotra.html?id=AnnapurnaStotra");
-  localStorage.setItem("🌼 श्री व्यंकटेश स्तोत्र | Vyankatesh Stotra","shree/stotra.html?id=VyankateshStotra");
-  localStorage.setItem("🌼 श्री नवग्रह स्तोत्र | Navgrah Stotra","shree/stotra.html?id=NavgrahStotra");
-  localStorage.setItem("🌼 श्री हनुमान चालीसा | Hanuman Chalisa","shree/stotra.html?id=HanumanChalisa");
-  localStorage.setItem("🌼 श्री मनाचे श्लोक | Manache Shlok","shree/stotra.html?id=ManacheShlok");
-  localStorage.setItem("🌼 श्री सूक्तम् | Shree Sukta","shree/stotra.html?id=ShreeSukta");
-  localStorage.setItem("🌼 पुरुष सूक्त | Purush Sukta","shree/stotra.html?id=PurushSukta");
-  localStorage.setItem("🌼 श्री महालक्ष्मी स्तोत्र | श्री महालक्ष्मी अष्टक | Mahalaxmi Stotra / Ashtak","shree/stotra.html?id=MahalaxmiStotra");
-  localStorage.setItem("🌼 श्री ​गणपती अथर्वशीर्ष | Ganapati Atharvashirsh","shree/stotra.html?id=GanapatiAtharvashirsh");
-  localStorage.setItem("🌼 श्री विष्णु सहस्रनाम स्तोत्रम् | Vishnu Sahastranam","shree/stotra.html?id=VishnuSahastranam");
-  localStorage.setItem("🌼 श्री महालक्ष्मी कवच | Mahalaxmi Kavach","shree/stotra.html?id=MahalaxmiKavach");
-  localStorage.setItem("🌼 श्री दुर्गा कवच | Mahalaxmi Kavach","shree/stotra.html?id=DurgaKavach");
-  localStorage.setItem("🌼 श्री स्वामी समर्थ तारक मंत्र | Swami Samarth Tarak Mantra","shree/stotra.html?id=SwamiSamarthTarakMantra");
+  localStorage.setItem("🌼 श्री अन्नपूर्णा स्तोत्र | Annapurna Stotra","shree/stotra.html?id=as");
+  localStorage.setItem("🌼 श्री व्यंकटेश स्तोत्र | Vyankatesh Stotra","shree/stotra.html?id=vys");
+  localStorage.setItem("🌼 श्री नवग्रह स्तोत्र | Navgrah Stotra","shree/stotra.html?id=ns");
+  localStorage.setItem("🌼 श्री हनुमान चालीसा | Hanuman Chalisa","shree/stotra.html?id=hc");
+  localStorage.setItem("🌼 श्री मनाचे श्लोक | Manache Shlok","shree/stotra.html?id=mncs");
+  localStorage.setItem("🌼 श्री सूक्तम् | Shree Sukta","shree/stotra.html?id=ss");
+  localStorage.setItem("🌼 पुरुष सूक्त | Purush Sukta","shree/stotra.html?id=ps");
+  localStorage.setItem("🌼 श्री महालक्ष्मी स्तोत्र | श्री महालक्ष्मी अष्टक | Mahalaxmi Stotra / Ashtak","shree/stotra.html?id=mxms");
+  localStorage.setItem("🌼 श्री ​गणपती अथर्वशीर्ष | Ganapati Atharvashirsh","shree/stotra.html?id=gash");
+  localStorage.setItem("🌼 श्री विष्णु सहस्रनाम स्तोत्रम् | Vishnu Sahastranam","shree/stotra.html?id=vs");
+  localStorage.setItem("🌼 श्री महालक्ष्मी कवच | Mahalaxmi Kavach","shree/stotra.html?id=mk");
+  localStorage.setItem("🌼 श्री दुर्गा कवच | Mahalaxmi Kavach","shree/stotra.html?id=dk");
+  localStorage.setItem("🌼 श्री स्वामी समर्थ तारक मंत्र | Swami Samarth Tarak Mantra","shree/stotra.html?id=sstm");
 
-  localStorage.setItem("🏵️ श्री सत्यनारायण कथा | Satyanarayan Katha","shree/stotra.html?id=SatyanarayanKatha");
+  localStorage.setItem("🏵️ श्री सत्यनारायण कथा | Satyanarayan Katha","shree/stotra.html?id=sk2");
   
 }
