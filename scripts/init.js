d865a8e753f8695cda75d4aeb938a3095699bfa9
Devendra Panchwagh <devendrapp@zohomail.in>
2025-11-26 08:15:15 +0530

    ++

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,6 @@
 function loadDefaultItems() {
   localStorage.setItem("0000_default_categories"
-    ,"🎺,🕉️,🚩,📰,📻,🖥️");
+    ,"🎺,🕉️,🚩,📰,📻,🖥️ Ⓜ️");
 
 localStorage.setItem("0000_quick_search_buttons",
   "📻,📰,🕉️,🎤,🎙️,🎧,🐤,🖥️,📀,▶️,Ⓜ️,<i class=\"material-icons\">h_mobiledata</i>,<i class=\"material-icons\">explicit</i>,🎬,🐘,🤣,💡,🇮🇳"
