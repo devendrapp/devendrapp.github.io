0e563767bbfa69066bdf9aa2a3b494410b1337e3
Devendra Panchwagh <devendrapp@zohomail.in>
2025-12-24 06:25:57 +0530

    ++

--- scripts/init.js
+++ scripts/init.js
@@ -1,8 +1,6 @@
 function loadDefaultItems() {
 
-localStorage.setItem("0000_quick_search_buttons",
-  "📻,📰,🕉️,🎵,🎤,🎧,🐤,🖥️,🐘,Ⓜ️,<i class=\"material-icons\">h_mobiledata</i>,<i class=\"material-icons\">explicit</i>,🎬,🎙️,📀,🤣,💡,🇮🇳"
-  );
+//localStorage.setItem("0000_quick_search_buttons","📻,📰,🕉️,🎵,🎤,🎧,🐤,🖥️,🐘,Ⓜ️,<i class=\"material-icons\">h_mobiledata</i>,<i class=\"material-icons\">explicit</i>,🎬,🎙️,📀,🤣,💡,🇮🇳");
 
 
   localStorage.setItem("🌺 श्री ​गणपती आरती | Ganapati Aarti 51000","shree/stotra.html?id=ga");
